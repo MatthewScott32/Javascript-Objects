7114f7cedda75da2ef6467e4384e863d9d646b94
Matthew Blagg <matthewscottblagg@gmail.com>
2019-10-09 14:42:16 -0500

    lightning exercises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 console.log("learning about objects");
+
+const car = {
+    color: "red",
+    year: "2015",
+    make: "Ford",
+    model: "Mustang",
+}
+console.log(car);
+
+const animals = [ "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"];
+console.log(animals);
+
+const dad = {
+    name: "Jim"
+}
+
+const mom = {
+    name: "Helen"
+}
+
+const sister = {
+    name: "Sarah"
+}
+
+const brother = {
+    name: "Bobby"
+}
+
+const familyMembers = [dad, mom, sister, brother]
+
+console.log("familyMembers", familyMembers);
